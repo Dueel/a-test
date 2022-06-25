3833b7ee18e110521efb853e08fe315b41ee8ea2
Dueel <105398949+Dueel@users.noreply.github.com>
2022-06-25 09:44:58 +0530

    3

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Duel</title>
+        <link rel="icon" href="/Dueel.ico" />
+      </Head>
+
+      <main>
+        <Header title="Hello! My name is Duel" />
+        <p className="description">
+          I am a newbie just learning how to code.
+        </p>
+      </main>
+
+    </div>
+  )
+}
